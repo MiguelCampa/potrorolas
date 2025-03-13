85e0a4e28f38095262daf53ff5815496e741ab3b
alumnog <alumnog@S921-7.itson.edu.mx>
2025-03-13 09:56:10 -0700

    Se agaregaron las clases entidades al modelo

---
+++ src/mx/itson/potrorolas/enums/Genero.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package mx.itson.potrorolas.enums;
+
+/**
+ *
+ * @author alumnog
+ */
+public enum Genero {
+    
+    POP,
+    ROCK,
+    DANCE,
+    SALSA,
+    BACHATA,
+    OTROS
+    
+    
+    
+    
+    
+    
+}
